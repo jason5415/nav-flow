00f4f7d856e2d6217f6d1822e89a4a50da07bb82
jaspur <995594631@qq.com>
2021-04-09 20:37:11 +0800

    Use this activity.

---
+++ app/src/main/java/ee5415/group3/nav_flow/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package ee5415.group3.nav_flow.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
